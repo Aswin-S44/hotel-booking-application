e945db31c4ae00cbc748b64e181f55a5193eb80c
vignesh5142 <vigneshm5142@gmail.com>
2026-02-12 20:57:20 +0530

    new

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,10 @@
 import "./config/env.js";
+import db from "./config/db.js";
 import express from "express";
 import cors from "cors";
-import { supabase } from "./config/supabaseClient.js";
-import bookingRoutes from "./routes/booking.routes.js";
 
 const app = express();
+db();
 
 app.use(cors({
     origin: process.env.FRONTEND_URL || "http://localhost:5173",
@@ -12,8 +12,7 @@ app.use(cors({
 }))
 app.use(express.json());
 
-app.use("/api/bookings", bookingRoutes);
-const port = process.env.PORT
+const port = process.env.PORT || 5000; 
 
 // Health check
 app.get('/api/health', (req, res) => {
